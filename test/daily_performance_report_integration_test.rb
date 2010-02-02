768631286a5e998d611fd19322e5630bfe61148d
Chris Roos <chrisroos@Chris-Rooss-MacBook-Pro-2.local>
2010-02-02 19:43:39 +0000

    Copied the report from 27th Jan 2010 into the, new, fixtures directory to keep it out of the data directory.

--- test/daily_performance_report_integration_test.rb
+++ test/daily_performance_report_integration_test.rb
@@ -3,11 +3,11 @@ require 'daily_performance_report'
 
 class DailyPerformanceReportIntegrationTest < Test::Unit::TestCase
   
-  DATA_DIR = File.join(File.dirname(__FILE__), '..', 'data')
+  FIXTURES_DIR = File.join(File.dirname(__FILE__), 'fixtures')
   
   def test_should_convert_the_daily_performance_report_from_html_into_csv
-    html_report      = File.read(File.join(DATA_DIR, 'se-2010-01-27.html'))
-    csv_report       = File.read(File.join(DATA_DIR, 'se-2010-01-27.csv'))
+    html_report      = File.read(File.join(FIXTURES_DIR, 'se-2010-01-27.html'))
+    csv_report       = File.read(File.join(FIXTURES_DIR, 'se-2010-01-27.csv'))
     generated_report = DailyPerformanceReport.new(html_report).to_csv
 
     original_lines  = csv_report.split("\n")
