05b290a234b9d8071e0d28f35735551bf3e01173
Chris Roos <chris@seagul.co.uk>
2010-12-17 16:41:38 +0000

    Moved the script to convert all files into bin.

--- convert-all-html-data.rb
+++
@@ -1,9 +0,0 @@
-Dir['data/html/*.html'].each do |file|
-  p file
-  
-  html_filename = File.basename(file)
-  csv_filename = html_filename.sub(/\.html/, '.csv')
-  
-  cmd = "ruby -Ilib ./bin/sedpr-to-csv #{file} > data/csv/#{csv_filename}"
-  `#{cmd}`
-end
