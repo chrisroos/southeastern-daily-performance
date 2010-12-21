1c913b7abf68e6a24cc4fed86395898ceecc0d10
Chris Roos <chris@seagul.co.uk>
2010-12-21 14:56:50 +0000

    I don't like having javascript in the html templates.

---
+++ javascripts/query-tool.js
@@ -0,0 +1,37 @@
+var FusionApiUrl  = 'http://www.google.com/fusiontables/api/query?sql=';
+var FusionTableID = 359310;
+
+var resetRawQuery = function() {
+  $('#rawQuery').val('SELECT * FROM ' + FusionTableID + ' LIMIT 5');
+}
+
+$(function() {
+  resetRawQuery();
+  
+  $('#queryReset').click(resetRawQuery);
+  
+  $('#queryEncoder').click(function() {
+    var rawQuery     = $('#rawQuery').val();
+    var escapedQuery = encodeURIComponent(rawQuery);
+    var url          = FusionApiUrl + escapedQuery;
+    
+    var anchor = $('<a href="' + url + '" target="_blank">View results as CSV</a>');
+    $('#escapedQuery').html(anchor);
+    
+    $.getJSON(url + '&jsonCallback=?', function(data) {
+      $('#queryResults').remove();
+      buildResultsTable(data, $('#escapedQuery'));
+    });
+  });
+  
+  $('.query').each(function() {
+    $(this).click(function() {
+      var querystring = this.search;
+      var escapedQuery = querystring.split('sql=')[1];
+      var unescapedQuery = decodeURIComponent(escapedQuery);
+      $('#rawQuery').val(unescapedQuery);
+      return false;
+    })
+  });
+  
+})
