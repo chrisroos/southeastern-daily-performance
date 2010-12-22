c8e56da93a80f2e43df5e38912a12f349024ced5
Chris Roos <chris@seagul.co.uk>
2010-12-22 14:01:12 +0000

    Modifying the generate-archives.rb script allows me to re-use it to generate a CSV archive too.
    * Added a couple of items to the todo list.

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ A simple web form that allows you to execute queries against the Southeastern da
 ## TODO
 
 * Make it easy to query the raw data from both tables
+* Find journeys that took over 30 minutes as they're refundable (i think)
+* Add a rake task to generate the archive index pages
+* update this readme
 
 * Move the data from the master branch into here.
 * Add a history of queries.
--- _additional/generate-archives.rb
+++ _additional/generate-archives.rb
@@ -3,20 +3,30 @@ require 'erb'
 
 site_root = File.expand_path('../../', __FILE__)
 html_dir  = File.join('data', 'html')
+csv_dir   = File.join('data', 'csv')
 
 first_date = Dir[File.join(site_root, html_dir, '*.html')].first
 first_date = Date.parse(first_date)
 last_date  = Date.today
 
 dates = (first_date..last_date).inject({}) do |hash, date|
+  html_file = File.join(html_dir, "#{date}.html")
+  csv_file  = File.join(csv_dir, "#{date}.csv")
   hash[date] = {
-    :exists => File.exists?(File.join(site_root, html_dir, "#{date}.html")),
-    :href   => File.join(html_dir, "#{date}.html")
+    :html_exists => File.exists?(File.join(site_root, html_file)),
+    :html_href   => html_file,
+    :csv_exists  => File.exists?(File.join(site_root, csv_file)),
+    :csv_href    => csv_file
   }
   hash
 end
 
 template = File.read(File.expand_path('../templates/raw-html-data.html.erb', __FILE__))
 File.open('../raw-html-data.html', 'w') do |file|
+  file.puts ERB.new(template, nil, '>').result
+end
+
+template = File.read(File.expand_path('../templates/raw-csv-data.html.erb', __FILE__))
+File.open('../raw-csv-data.html', 'w') do |file|
   file.puts ERB.new(template, nil, '>').result
 end
